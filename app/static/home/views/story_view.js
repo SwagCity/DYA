04bbb0d1d6112854a8e39e17a5e206a7c727cc4c
Genji Noguchi <genji.noguchi@gmail.com>
2015-05-19 01:31:44 -0400

    Some form of tree node children stuff working; some quirks need to be worked out.

--- app/static/home/views/story_view.js
+++ app/static/home/views/story_view.js
@@ -5,19 +5,29 @@
  */
 
 /* A single node on the Story tree */
-App.Views.Story = Marionette.CompositeView.extend({
+App.Views.StoryNode = Marionette.CompositeView.extend({
 	template : "#snippet-template",
-
-	tagName : "ul",
 
 	initialize : function() {
 		// By not specifying the child view type, the CompositeView defaults to using itself as the child view.
 		this.collection = this.model.children;
+
+		this.el.id = this.model.attributes._id;
 	},
 
-	appendHtml : function(collectionView, itemView) {
+	// Callbacks on actions in the DOM
+	events : {
 
-	}
+	},
+	toggleChildView : function() {
+
+	},
+
+	// Callbacks on changes to the Backbone collections
+	collectionEvents : {
+		"add" : "modelAdded",
+	},
+	modelAdded : function() {}
 })
 
 App.Views.StoryTree = Marionette.CollectionView.extend({
