b90e294dba80839b117dafe91d5c88809df29aa2
Angela Lin <angela97lin@gmail.com>
2015-05-20 13:30:46 -0400

    circles not appearing

--- app/static/libs/svg.js
+++ app/static/libs/svg.js
@@ -1,18 +1,48 @@
+var circles = [];
+
+var mouseClick = function(evt){
+    console.log("hello");
+    var circle = evt.target;
+    var cRadius = circle.getAttribute("r");
+    if (cRadius < 100)
+	circle.setAttribute("r", '120');
+    else
+	circle.setAttribute("r", '70');
+}
 
 
-var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-var svgNS = svg.namespaceURI;
+var data = [140,340,540];
 
-var circle = document.createElementNS(svgNS, 'circle');
-circle.attr("cx", 30)
-circle.attr("cy", 30)
-circle.attr("r", 20)
-circle.on("click", mouseClick);
 
-svg.appendChild(cirle);
-document.body.appendChild(svg);
 
-var h=document.createElement('a');
-var t=document.createtextNode('Hello');
-h.appendChild(t);
-document.body.appendChild(h);
+var drawCircle = function(x,y,r){
+    return {
+	x:x,
+	
+	drawC : function(){
+	    var circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
+	    circle.setAttributeNS(null,'cx', x);
+	    circle.setAttributeNS(null,'cy', '70');
+	    circle.setAttributeNS(null,'r', '70');	    
+	    circle.setAttributeNS(null,'fill','#95B3D7');
+	    circle.setAttributeNS(null,'visibility','visible');
+	    circle.addEventListener("click",mouseClick);
+	    
+	    console.log('hello');
+	}	
+    }
+}
+
+
+
+function draw(){
+    for (var i = 0; i < data.length; i ++){
+	circles.push(drawCircle(data[i]));
+	console.log('wassup');
+    }
+    for (var i =0 ; i <circles.length;i++){
+	circles[i].drawC()
+    }
+}
+
+$( document ).ready( draw );
