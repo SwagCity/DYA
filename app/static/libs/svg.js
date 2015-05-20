1ec51e52e849011aa8861ba828ff0aa25d65e0af
Angela Lin <angela97lin@gmail.com>
2015-05-20 14:41:45 -0400

    only one circle can enlarge

--- app/static/libs/svg.js
+++ app/static/libs/svg.js
@@ -1,51 +1,40 @@
 var circles = [];
+var s = [];
+
 
 var mouseClick = function(evt){
-    console.log("hello");
     var circle = evt.target;
-    var cRadius = circle.getAttribute("r");
-    if (cRadius < 100)
-	circle.setAttribute("r", '120');
-    else
-	circle.setAttribute("r", '70');
+    for (i=0;i<circles.length;i++){
+	var current = s[i];
+	if (current == circle)
+	    current.setAttribute('r','120');
+	    
+	else
+	    current.setAttribute('r','70');
+    }
 }
 
-function draw() {
-    var svg = document.getElementsByTagName('svg')[0];
-    var svgNS = svg.namespaceURI;
-
-    var rect = document.createElementNS(svgNS,'rect');
-    rect.setAttribute('x',5);
-    rect.setAttribute('y',5);
-    rect.setAttribute('width',100);
-    rect.setAttribute('height',36);
-    rect.setAttribute('fill','#95B3D7');
-
-    svg.appendChild(rect);
-}
 
 var data = [140,340,540];
 
 
-
-var drawCircle = function(x,y,r){
+var drawCircle = function(y){
     return {
-	x:x,
+	y:y,
 	
 	drawC : function(){
 	    var svg = document.getElementsByTagName('svg')[0];
 	    var svgNS = svg.namespaceURI;
 
 	    var circle = document.createElementNS(svgNS,'circle');
-	    circle.setAttribute('cx', x);
-	    circle.setAttribute('cy', '70');
+	    circle.setAttribute('cx', '170');
+	    circle.setAttribute('cy', y);
 	    circle.setAttribute('r', '70');	    
 	    circle.setAttribute('fill','#95B3D7');
 	    circle.addEventListener("click",mouseClick);
-	    
+	    s.push(circle);
 	    svg.appendChild(circle);
-	    console.log('hello');
-	}	
+	},
     }
 }
 
@@ -54,7 +43,6 @@ var drawCircle = function(x,y,r){
 function drawf(){
     for (var i = 0; i < data.length; i ++){
 	circles.push(drawCircle(data[i]));
-	console.log('wassup');
     }
     for (var i =0 ; i <circles.length;i++){
 	circles[i].drawC()
