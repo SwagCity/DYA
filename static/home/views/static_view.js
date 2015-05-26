c096cd007be8360559756e24d06371116a6a6878
Genji Noguchi <genji.noguchi@gmail.com>
2015-05-26 08:57:17 -0400

    fixed annoying wrapper div tag in Marionette views

--- static/home/views/static_view.js
+++ static/home/views/static_view.js
@@ -3,5 +3,19 @@ App.Views.HomeView = Marionette.ItemView.extend({
 })
 
 App.Views.EditView = Marionette.ItemView.extend({
-	template : "#edit-template"
+	template : "#edit-template",
+
+	onRender : function() {
+
+		console.log('onRender');
+
+		// Get rid of the wrapping div
+		// tip from codepen.io/somethingkindawierd/pen/txnpE
+		this.$el = this.$el.children();
+
+		//Unwrap the elemnt to prevetn infinitely nesting elements during re-render
+		this.$el.unwrap();
+		this.setElement(this.$el);
+
+	}
 })
