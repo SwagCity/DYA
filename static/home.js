d7631e35eb86a43c61bbaf7bc1ac73b6cf52a729
kig12345 <kig-12345@hotmail.com>
2015-05-26 23:49:26 -0400

    some styling

---
+++ static/home.js
@@ -0,0 +1,26 @@
+
+storyJSON = d3.json("../static/stories.json", function(error, storyData){
+    
+    var totalStories = 0;
+    
+    function visit(parent, visitFn, childrenFn) {
+        if (!parent) return;
+
+        visitFn(parent);
+
+        var children = childrenFn(parent);
+        if (children) {
+            var count = children.length;
+            for (var i = 0; i < count; i++) {
+                visit(children[i], visitFn, childrenFn);
+            }
+        }
+    }
+
+    visit(storyData, function(d) {
+	totalStories++;
+    });
+
+    console.log(totalStories);
+	  
+}
