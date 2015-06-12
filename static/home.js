c4a28880bf492b079a753ab7eeb8f688bfc3fae3
kig12345 <kig-12345@hotmail.com>
2015-06-12 13:20:18 -0400

    lol

--- static/home.js
+++ static/home.js
@@ -33,18 +33,17 @@ storyJSON = d3.json("../static/stories.JSON", function(error, storyData){
     
     function draw(data) {
 	var lists = d3.select("body").selectAll("ul")
-	    .data(data,function(d,i) {console.log(d); return d; });
+	    .data(data);
 	
 	lists.enter().append("ul")
-	    .attr("class",function(d) { return d.key });
 	
 	lists.exit().remove();
 
 	var lines = lists.selectAll("li")
-	    .data(function(d) { return d}, function(d,i) { console.log(d.values); return d.values; });
+	    .data(data);
 	
 	lines.enter().append("li")
-	    .text(function(d, i) { console.log(d);return d; });
+	    .text(function(d) { return d.values; });
 	
 	lines.exit().remove();
     }
