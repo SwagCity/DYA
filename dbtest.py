7074ac5a0788d28d3b6e189d38a8c1b912c56cfb
d-tsui <dtsui918@yahoo.com>
2015-06-15 22:07:08 -0400

    dbtest

---
+++ dbtest.py
@@ -0,0 +1,28 @@
+#db test
+
+import db
+
+def run():
+    x = db.s_add("The Store","One fine day, Mark goes to the store.", None, {"author":"Bob","tags":["retail","adventure"]})
+    print x
+    print db.s_get(x)
+    print ""
+    print "update"
+    db.s_edit(x, "The Store is a Lie", None, None, [])
+    print db.s_get(x)
+
+    y = db.s_add("Walking in the store","He finds a wide variety of goods", x, {"author":"Mark","tags":["retail","adventure"]})
+    print ""
+    print "SEARCH"
+    print db.s_search("store","title")
+    '''
+    print db.s_getall()
+    db.s_delete(y)
+    print ""
+    print "ALL"
+    print db.s_getall()
+    '''
+
+
+if __name__ == '__main__':
+    run()
